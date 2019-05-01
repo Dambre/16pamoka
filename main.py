b0d02b99bb9bfd39c4406cc37d6a50bf8adf2ea0
Lukas <lukas.dambr@gmail.com>
2019-05-01 22:38:51 +0300

    get, post, cookies

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 
-from flask import Flask
+from flask import Flask, render_template, make_response, request
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
-    return 'Hello world !'
+    response = make_response(render_template('index.html'))
+    response.set_cookie("user_name", expires=0)
+    return response
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        response = make_response(
+            render_template("login-success.html", username=username),
+        )
+        expiration_day = datetime.now() + timedelta(days=10)
+        response.set_cookie("username", username, expires=expiration_day)
+        return response
+    else:
+        return render_template('/login.html')
+
+
+@app.route('/about-me')
+def about_me():
+    now = datetime.now()
+    return render_template('about_me.html', dabar=now)
+
+
+@app.route('/portfolio')
+def portfolio():
+    now = datetime.now()
+    return render_template('portfolio.html', dabar=now)
 
 
 if __name__ == '__main__':
